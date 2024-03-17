b73b45484fe4a61af37e021a7f38f65ad92764e7
VinhTieng <vinhtieng123@gmail.com>
2024-03-17 22:20:27 +0700

    Update handleEvents function and main window creation

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
-const {
-  app,
-  BrowserWindow,
-  ipcMain,
-  Notification,
-  shell,
-  systemPreferences,
-  screen,
-} = require("electron");
 require('dotenv').config()
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const { setup: setupPushReceiver } = require("electron-push-receiver");
 const url = require('url');
+
 const handleEvents = require("./handle-event");
 const { EVENTS } = require("./events");
 const { APP_URL } = require("./config");
+
 let mainWindow;
 
 // Set deep links
@@ -28,7 +22,6 @@ if (process.defaultApp) {
 }
 
 const gotTheLock = app.requestSingleInstanceLock();
-
 if (!gotTheLock) {
   app.quit();
 } else {
@@ -44,7 +37,6 @@ if (!gotTheLock) {
   app.whenReady().then(() => {
     createWindow();
     app.setAppUserModelId("middo")
-    checkPermission();
     app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
@@ -64,13 +56,10 @@ function loginCallback(urlStr) {
 }
 
 function createWindow() {
-  const screenSize = screen.getPrimaryDisplay().workAreaSize;
   mainWindow = new BrowserWindow({
     title: "Middo",
     // backgroundColor: '#2e2c29',
     icon: path.join(__dirname, "src", "assets", "icon.ico"),
-    width: screenSize.width,
-    height: screenSize.height,
     // alwaysOnTop: true,
     webPreferences: {
       contextIsolation: true,
@@ -79,64 +68,15 @@ function createWindow() {
     },
   });
   // Hide menu bar
-  // mainWindow.setMenu(null);
+  mainWindow.setMenu(null);
+  mainWindow.maximize();
   mainWindow.loadURL(APP_URL);
   // mainWindow.webContents.openDevTools();
   setupPushReceiver(mainWindow.webContents);
-  // checkPermission();
+  handleEvents(mainWindow);
 }
 
-handleEvents(mainWindow);
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-ipcMain.on('set-ignore-mouse-events', (event, ignore, options) => {
-  const win = BrowserWindow.fromWebContents(event.sender)
-  win.setIgnoreMouseEvents(ignore, options)
-})
-
-ipcMain.on(EVENTS.TOGGLE_MIC, () => {
-  mainWindow.webContents.send(EVENTS.TOGGLE_MIC);
-});
-ipcMain.on(EVENTS.TOGGLE_CAMERA, () => {
-  mainWindow.webContents.send(EVENTS.TOGGLE_CAMERA);
-});
-ipcMain.on(EVENTS.STOP_SHARE, () => {
-  mainWindow.webContents.send(EVENTS.STOP_SHARE);
-});
-
-// Check and get permission for access mic and camera
-const checkPermission = async () => {
-  // const micPermission = await systemPreferences.askForMediaAccess("microphone");
-  // const cameraPermission = await systemPreferences.askForMediaAccess("camera");
-  // if(!micPermission) {
-  //   shell.openExternal(
-  //     "x-apple.systempreferences:com.apple.preference.security?Privacy_Microphone"
-  //   );
-  // } else if(!cameraPermission) {
-  //   shell.openExternal(
-  //     "x-apple.systempreferences:com.apple.preference.security?Privacy_Camera"
-  //   );
-  // }
-}
-
-
-ipcMain.on(EVENTS.SHOW_NOTIFICATION, (e, data) => {
-  // Check is focused
-  const isFocused = mainWindow.isFocused();
-  if(isFocused) return;
-  const {title, body, url} = data;
-  const myNotification = new Notification({ 
-    title, 
-    body,
-    sound: path.join(__dirname, 'assets', 'notification.mp3'),
-    icon: path.join(__dirname, 'assets', 'icon.png'),
-    silent: false
-  });
-  myNotification.onclick = () => {
-    console.log('Notification clicked::', url);
-    myNotification.close();
-  }
-  myNotification.show();
-});
