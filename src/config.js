f96557e7e39c22f375f929bb4a3d7d7ee44bd034
VinhTieng <vinhtieng123@gmail.com>
2024-04-08 08:30:21 +0700

    Update config.js to use environment variables

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports = {
-    APP_URL: "https://test.middo.app",
+    APP_URL: process.env.APP_URL,
     APPLE_ID: process.env.APPLE_ID,
     APPLE_PASSWORD: process.env.APPLE_ID_PASSWORD,
     APPLE_TEAM_ID: process.env.TEAM_ID,
-    SENDER_ID: "835558552712",
+    SENDER_ID: process.env.SENDER_ID,
     APP_TITLE: 'Middo',
     IS_MAC: process.platform === "darwin",
     DEEP_LINK: 'middo',
